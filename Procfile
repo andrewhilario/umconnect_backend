6391d741780ed489a3183eb5668ff8fb6e6fdabe
Drew <hilarioandrew12@gmail.com>
2024-04-14 23:08:57 +0800

    Update Django version to 4.0 in requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'umconnect_backend.wsgi'
--- umconnect_backend/settings.py
+++ umconnect_backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = "django-insecure-6mkmujmhf7-9t704u15zqu8xxi6ddfe6%w8p%lmp2j8^11(5o%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
+ALLOWED_HOST = ["*"]
 
 
 # Application definition
@@ -185,9 +185,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
