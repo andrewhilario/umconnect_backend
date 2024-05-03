6aa90a8ac6b6f76d92bae451bffb5e4e554f0978
Drew <hilarioandrew12@gmail.com>
2024-05-03 22:58:04 +0800

    Update build_files.sh to remove static file collection step

--- build_files.sh
+++ build_files.sh
@@ -12,6 +12,3 @@ python3 -m pip install -r requirements.txt
 echo "Migrating Database..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput
