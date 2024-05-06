d058173f477b2dc02d5b191f75596484e1fb2c08
Drew <hilarioandrew12@gmail.com>
2024-05-06 17:35:52 +0800

    refactor: Fix typo in DeleteStoriesAfter24Hours view

--- stories/views.py
+++ stories/views.py
@@ -69,7 +69,14 @@ class CreateStoryView(APIView):
 
 class DeleteStoriesAfter24Hours(APIView):
     def get(self, request):
-        Stories.objects.filter(
-            created_at__lte=timezone.now() - timedelta(days=1)
-        ).delete()
-        return Response({"message": "Stories older than 24 hours have been deleted."})
+        if Stories.objects.filter(
+            reated_at__lte=timezone.now() - timedelta(days=1)
+        ).exist():
+            Stories.objects.filter(
+                created_at__lte=timezone.now() - timedelta(days=1)
+            ).delete()
+            return Response(
+                {"message": "Stories older than 24 hours have been deleted."}
+            )
+
+        return Response({"message": "No stories to delete."})
